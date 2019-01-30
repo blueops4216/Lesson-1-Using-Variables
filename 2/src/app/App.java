84d807a07d9f706a2846baef8625182cfc6ff0fe
Kevin Furlong <kevinmfurlong@gmail.com>
2019-01-29 21:16:42 -0500

    Added seconds lesson

---
+++ 2/src/app/App.java
@@ -0,0 +1,23 @@
+package app;
+
+/* Write a program that stores your name and year of graduation into variables,
+ * and displays their values on the screen.
+ *
+ * Make sure that you use two variables, and that the variable that holds your
+ * name is the best type for such a variable, and that the variable that holds
+ * the year is the best type for that variable.
+ * 
+ * Also make sure that your variable names are good: the name of a variable should
+ * always relate to its contents.
+ *
+ * Your output should look like:
+ * My name is Juan Valdez and I'll graduate in 2010.
+ * 
+ * Notice that in the example above, the values "Juan Valdez" and 2010 have been
+ * stored in variables before printing. 
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Your code here
+    }
+}

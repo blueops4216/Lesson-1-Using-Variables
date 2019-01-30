e41a5b4413accbac48a9eb7cad488a2907c1397d
Kevin Furlong <kevinmfurlong@gmail.com>
2019-01-29 21:21:15 -0500

    Added lesson 3

---
+++ 3/src/app/App.java
@@ -0,0 +1,34 @@
+package app;
+
+/* Your Schedule
+ *
+ * Use several variables to store the names of your classes and their teachers.
+ * Then, display a nice little table displaying your schedule.	Just FYI, my
+ * column of courses has a width of 26 characters, and the teacher column has a
+ * width of 15. The first and last lines are a plus sign, fifty dashes
+ * (a.k.a. minus signs) and another plus sign.
+ *
+ * Your table doesn't need to look exactly like this, or even line up. I used a
+ * total of sixteen variables (course1, course2, ... course8, teacher1, teacher2, etc.).
+ * You should do the same.
+ *
+ * Your output should look like:
+ *
+ * +------------------------------------------------------------+
+ * | 1 |                          English III |       Ms. Lapan |
+ * | 2 |                          Precalculus |     Mrs. Gideon |
+ * | 3 |                         Music Theory |       Mr. Davis |
+ * | 4 |                        Biotechnology |      Ms. Palmer |
+ * | 5 |           Principles of Technology I |      Ms. Garcia |
+ * | 6 |                             Latin II |    Mrs. Barnett |
+ * | 7 |                        AP US History | Ms. Johannessen |
+ * | 8 | Business Computer Infomation Systems |       Mr. James |
+ * +------------------------------------------------------------+
+ *
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // You code here
+    }
+}

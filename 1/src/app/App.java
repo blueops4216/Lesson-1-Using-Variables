6f57442bccb5ce3d17327cdc87bf3618a4fcb47f
Kevin Furlong <kevinmfurlong@gmail.com>
2019-01-29 21:09:16 -0500

    Initial commit

---
+++ 1/src/app/App.java
@@ -0,0 +1,21 @@
+package app;
+
+
+/*
+* Write a program that creates three variables: an int, a double, and a String.
+*
+* Put the value 113 into the first variable, the value 2.71828 into the second,
+* and the value "Computer Science" into the third. It does not matter what you
+* call the variables... this time.
+* 
+* Then, display the values of these three variables on the screen, one per line.
+* 
+* This is room # 113 
+* e is close to 2.71828 
+* I am learning a bit about Computer Science 
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Your code goes here.
+    }
+}
